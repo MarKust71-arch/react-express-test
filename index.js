427d15e0e4426857d6b91d41447f1f6a0263d540
unknown <markust71@hotmail.com>
2020-05-10 12:52:09 +0200

    basic React

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const host = 'localhost';
-const port = 3000;
+const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
